94fd07949fef70a91ff0b4d3851d89fd81abc030
alex <you@example.com>
2026-01-17 20:08:23 -0600

    modified:   ani.js

--- ani.js
+++ ani.js
@@ -1,5 +1,6 @@
 const WIDTH = 800;
 const HEIGHT = 800;
+const FPS = 60;
 
 let canvas = document.getElementById('canv')
 let ctx = canvas.getContext("2d")
@@ -10,7 +11,7 @@ function clearScreen(){
 }
 
 function point({x, y}){
-    const dimension = 20
+    const dimension = 10
     ctx.fillStyle = 'green'
     ctx.fillRect(x - dimension/2, y - dimension/2, dimension, dimension)
     console.log(x - dimension/2, y- dimension/2)
@@ -21,14 +22,56 @@ function normalizeCoord(point){
 return({
     // 
     x: (point.x + 1)/2 * WIDTH,
-    y: (1- (point.y + 1)/2) * HEIGHT
+    y: (1- (point.y + 1)/2) * HEIGHT,
 })
 }
 
 function project({x,y,z}){
 
-    
+    projectX = x/z;
+    projectY = y/z;
+
+    return {x: projectX, y: projectY}
 }
 //main call
-clearScreen()
-point(normalizeCoord({x:1,y:1}))
+
+let verts = [
+    {x:-.5,y:-.5,z:1},
+    {x:-.5,y:.5,z:1},
+    {x:.5,y:-.5,z:1},
+    {x:.5,y:.5,z:1}
+
+]
+
+function frame({x,y,z}){
+    point(normalizeCoord(project({x,y,z})))
+}
+
+function draw(){
+    clearScreen()
+ for(let i = 0; i < verts.length; i++){
+    frame(verts[i])
+ }
+}
+
+function change_z(vert){
+    vert.z += 1;
+    return vert
+}
+
+function rotate({x,y}){
+    let angle = 5;
+return{
+    x: x * Math.cos(angle) - y * Math.sin(angle),
+    y: x * Math.sin(angle) + y * Math.cos(angle)
+}
+}
+
+function play(){
+let delay = 1/FPS
+draw();
+setTimeout(play,delay)
+
+}
+
+play()
